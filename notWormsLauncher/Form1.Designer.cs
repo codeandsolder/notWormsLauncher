5ee2fffc4407e05f1acabcd307b28b9053c9c2f3
Code and Solder <codeandsolder@gmail.com>
2019-01-21 23:16:55 +0100

    windowsForms is evil
    C# is OK
    should have used WPF
    or Python

--- notWormsLauncher/Form1.Designer.cs
+++ notWormsLauncher/Form1.Designer.cs
@@ -28,12 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.connectButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addressBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nicknameBox = new System.Windows.Forms.TextBox();
+            this.nicknameButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(481, 12);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(81, 31);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "Apply";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(12, 203);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(550, 279);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server state:";
+            // 
+            // addressBox
+            // 
+            this.addressBox.Location = new System.Drawing.Point(177, 12);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(289, 31);
+            this.addressBox.TabIndex = 3;
+            this.addressBox.Text = "localhost:8000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Server address:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Available players:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nickname:";
+            // 
+            // nicknameBox
+            // 
+            this.nicknameBox.Location = new System.Drawing.Point(177, 95);
+            this.nicknameBox.Name = "nicknameBox";
+            this.nicknameBox.Size = new System.Drawing.Size(289, 31);
+            this.nicknameBox.TabIndex = 7;
+            // 
+            // nicknameButton
+            // 
+            this.nicknameButton.Location = new System.Drawing.Point(481, 95);
+            this.nicknameButton.Name = "nicknameButton";
+            this.nicknameButton.Size = new System.Drawing.Size(81, 31);
+            this.nicknameButton.TabIndex = 8;
+            this.nicknameButton.Text = "Apply";
+            this.nicknameButton.UseVisualStyleBackColor = true;
+            this.nicknameButton.Click += new System.EventHandler(this.nicknameButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(177, 133);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(168, 29);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "ready to play";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(438, 488);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 38);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Invite";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(574, 535);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.nicknameButton);
+            this.Controls.Add(this.nicknameBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.addressBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.connectButton);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "notWormsLauncher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox nicknameBox;
+        private System.Windows.Forms.Button nicknameButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- notWormsLauncher/Form1.cs
+++ notWormsLauncher/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WebSocketSharp;
 
 namespace notWormsLauncher
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
+
+        private WebSocket client;
+        const string hostPre = "ws://";
+
+        public Form1() {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+        }
+
+        private void connectButton_Click(object sender, EventArgs e){
+            string host = hostPre + addressBox.Text;
+            client = new WebSocket(host);
+            client.OnOpen += (ss, ee) => listBox1.Items.Add(string.Format("Connected to {0} successfully ", host));
+            client.OnError += (ss, ee) => listBox1.Items.Add("Error: " + ee.Message);
+            client.OnMessage += (ss, ee) => MessageBox.Show("Echo: " + ee.Data);
+            client.OnClose += (ss, ee) => listBox1.Items.Add(string.Format("Disconnected with {0}", host));
+            client.Connect();
+        }
+
+        private void nicknameButton_Click(object sender, EventArgs e)
+        {
+            var content = nicknameBox.Text;
+            if (!string.IsNullOrEmpty(content))
+                client.Send(content);
         }
     }
 }
