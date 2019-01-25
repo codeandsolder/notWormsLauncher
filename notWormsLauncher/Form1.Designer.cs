3db63ce3bbaa3690fc44d13bffdb162e6419941f
Code and Solder <codeandsolder@gmail.com>
2019-01-25 04:04:35 +0100

    basic client functionality

--- notWormsLauncher/Form1.Designer.cs
+++ notWormsLauncher/Form1.Designer.cs
@@ -30,136 +30,148 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.connectButton = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.clientListbox = new System.Windows.Forms.ListBox();
+            this.serverLabel = new System.Windows.Forms.Label();
             this.addressBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.nicknameBox = new System.Windows.Forms.TextBox();
             this.nicknameButton = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.availableCheckbox = new System.Windows.Forms.CheckBox();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // connectButton
             // 
-            this.connectButton.Location = new System.Drawing.Point(481, 12);
+            this.connectButton.Location = new System.Drawing.Point(247, 30);
+            this.connectButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.connectButton.Name = "connectButton";
-            this.connectButton.Size = new System.Drawing.Size(81, 31);
+            this.connectButton.Size = new System.Drawing.Size(75, 22);
             this.connectButton.TabIndex = 0;
-            this.connectButton.Text = "Apply";
+            this.connectButton.Text = "Connect";
             this.connectButton.UseVisualStyleBackColor = true;
             this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
             // 
-            // listBox1
+            // clientListbox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(12, 203);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(550, 279);
-            this.listBox1.TabIndex = 1;
+            this.clientListbox.FormattingEnabled = true;
+            this.clientListbox.Location = new System.Drawing.Point(11, 119);
+            this.clientListbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.clientListbox.Name = "clientListbox";
+            this.clientListbox.Size = new System.Drawing.Size(311, 147);
+            this.clientListbox.TabIndex = 1;
             // 
-            // label1
+            // serverLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(172, 46);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 25);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Server state:";
+            this.serverLabel.AutoSize = true;
+            this.serverLabel.Location = new System.Drawing.Point(93, 28);
+            this.serverLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.serverLabel.Name = "serverLabel";
+            this.serverLabel.Size = new System.Drawing.Size(134, 13);
+            this.serverLabel.TabIndex = 2;
+            this.serverLabel.Text = "Server state: disconnected";
             // 
             // addressBox
             // 
-            this.addressBox.Location = new System.Drawing.Point(177, 12);
+            this.addressBox.Location = new System.Drawing.Point(96, 6);
+            this.addressBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.addressBox.Name = "addressBox";
-            this.addressBox.Size = new System.Drawing.Size(289, 31);
+            this.addressBox.Size = new System.Drawing.Size(226, 20);
             this.addressBox.TabIndex = 3;
             this.addressBox.Text = "localhost:8000";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 15);
+            this.label2.Location = new System.Drawing.Point(11, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(164, 25);
+            this.label2.Size = new System.Drawing.Size(81, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Server address:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 175);
+            this.label3.Location = new System.Drawing.Point(11, 104);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(182, 25);
+            this.label3.Size = new System.Drawing.Size(89, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Available players:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(58, 98);
+            this.label4.Location = new System.Drawing.Point(34, 65);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(113, 25);
+            this.label4.Size = new System.Drawing.Size(58, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Nickname:";
             // 
             // nicknameBox
             // 
-            this.nicknameBox.Location = new System.Drawing.Point(177, 95);
+            this.nicknameBox.Location = new System.Drawing.Point(96, 61);
+            this.nicknameBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.nicknameBox.Name = "nicknameBox";
-            this.nicknameBox.Size = new System.Drawing.Size(289, 31);
+            this.nicknameBox.Size = new System.Drawing.Size(163, 20);
             this.nicknameBox.TabIndex = 7;
             // 
             // nicknameButton
             // 
-            this.nicknameButton.Location = new System.Drawing.Point(481, 95);
+            this.nicknameButton.Location = new System.Drawing.Point(263, 60);
+            this.nicknameButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.nicknameButton.Name = "nicknameButton";
-            this.nicknameButton.Size = new System.Drawing.Size(81, 31);
+            this.nicknameButton.Size = new System.Drawing.Size(59, 22);
             this.nicknameButton.TabIndex = 8;
-            this.nicknameButton.Text = "Apply";
+            this.nicknameButton.Text = "Set";
             this.nicknameButton.UseVisualStyleBackColor = true;
             this.nicknameButton.Click += new System.EventHandler(this.nicknameButton_Click);
             // 
-            // checkBox1
+            // availableCheckbox
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(177, 133);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(168, 29);
-            this.checkBox1.TabIndex = 9;
-            this.checkBox1.Text = "ready to play";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.availableCheckbox.AutoSize = true;
+            this.availableCheckbox.Location = new System.Drawing.Point(96, 85);
+            this.availableCheckbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.availableCheckbox.Name = "availableCheckbox";
+            this.availableCheckbox.Size = new System.Drawing.Size(86, 17);
+            this.availableCheckbox.TabIndex = 9;
+            this.availableCheckbox.Text = "ready to play";
+            this.availableCheckbox.UseVisualStyleBackColor = true;
+            this.availableCheckbox.CheckedChanged += new System.EventHandler(this.availableCheckbox_CheckedChanged);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(438, 488);
+            this.button3.Location = new System.Drawing.Point(260, 270);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(124, 38);
+            this.button3.Size = new System.Drawing.Size(62, 20);
             this.button3.TabIndex = 10;
             this.button3.Text = "Invite";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 535);
+            this.ClientSize = new System.Drawing.Size(333, 298);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.availableCheckbox);
             this.Controls.Add(this.nicknameButton);
             this.Controls.Add(this.nicknameBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.addressBox);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.serverLabel);
+            this.Controls.Add(this.clientListbox);
             this.Controls.Add(this.connectButton);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "notWormsLauncher";
@@ -172,15 +184,15 @@
         #endregion
 
         private System.Windows.Forms.Button connectButton;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox clientListbox;
+        private System.Windows.Forms.Label serverLabel;
         private System.Windows.Forms.TextBox addressBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox nicknameBox;
         private System.Windows.Forms.Button nicknameButton;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox availableCheckbox;
         private System.Windows.Forms.Button button3;
     }
 }
--- notWormsLauncher/Form1.cs
+++ notWormsLauncher/Form1.cs
@@ -15,30 +15,97 @@ namespace notWormsLauncher
     {
 
         private WebSocket client;
+        private bool connected = false;
         const string hostPre = "ws://";
 
         public Form1() {
             InitializeComponent();
         }
 
+        private void handleClosed() {
+            connected = false;
+            this.Invoke((MethodInvoker)delegate {
+                this.serverLabel.Text = "Server state: disconnected";
+                this.connectButton.Text = "Connect";
+            });
+        }
+
+        private void handleConnected() {
+            connected = true;
+            sendAvailability();
+            this.Invoke((MethodInvoker)delegate {
+                this.serverLabel.Text = "Server state: connected";
+                this.connectButton.Text = "Disconnect";
+            });
+        }
+        private bool trySetName() {
+            string name = nicknameBox.Text;
+            if (name.Contains('/')) {
+                MessageBox.Show("Invalid character in name: /");
+                return false;
+            }
+            if (clientListbox.Items.Contains(name)) {
+                MessageBox.Show("Name is in use!");
+                return false;
+            }
+            if (!string.IsNullOrEmpty(name) && connected) {
+                client.Send("n " + name);
+                return true;
+            }
+            return false;
+        }
+
+        private void sendAvailability() {
+            if (connected) {
+                client.Send("a " + availableCheckbox.Checked);
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e) {
         }
-
+        private void handleMessage(MessageEventArgs a) {
+            Console.WriteLine(a.Data);
+            string arguments = a.Data.Substring(2);
+            switch (a.Data[0]) {
+                case 't':
+                    if(!trySetName())
+                        this.Invoke((MethodInvoker)delegate {
+                            this.nicknameBox.Text = arguments;
+                        });
+                    break;
+                case 'l':
+                    this.Invoke((MethodInvoker)delegate {
+                        this.clientListbox.Items.Clear();
+                    });
+                    foreach (string player in arguments.Split('/')) {
+                        if (player != nicknameBox.Text)
+                            this.Invoke((MethodInvoker)delegate {
+                                this.clientListbox.Items.Add(player);
+                            });
+                    }
+                    break;
+            }
+        }
         private void connectButton_Click(object sender, EventArgs e){
-            string host = hostPre + addressBox.Text;
-            client = new WebSocket(host);
-            client.OnOpen += (ss, ee) => listBox1.Items.Add(string.Format("Connected to {0} successfully ", host));
-            client.OnError += (ss, ee) => listBox1.Items.Add("Error: " + ee.Message);
-            client.OnMessage += (ss, ee) => MessageBox.Show("Echo: " + ee.Data);
-            client.OnClose += (ss, ee) => listBox1.Items.Add(string.Format("Disconnected with {0}", host));
-            client.Connect();
+            if (!connected) {
+                string host = hostPre + addressBox.Text;
+                client = new WebSocket(host);
+                client.OnOpen += (ss, ee) => handleConnected();
+                client.OnError += (ss, ee) => Console.WriteLine("Error: " + ee.Message);
+                client.OnMessage += (ss, ee) => handleMessage(ee);
+                client.OnClose += (ss, ee) => handleClosed();
+                client.Connect();
+            } else {
+                client.Close();
+            }
         }
 
-        private void nicknameButton_Click(object sender, EventArgs e)
-        {
-            var content = nicknameBox.Text;
-            if (!string.IsNullOrEmpty(content))
-                client.Send(content);
+        private void nicknameButton_Click(object sender, EventArgs e) {
+            trySetName();
+        }
+        
+        private void availableCheckbox_CheckedChanged(object sender, EventArgs e) {
+            sendAvailability();
         }
     }
 }
